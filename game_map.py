79547b1ec088df9c92316d9a5f65d829410f2cec
NamesNotRich <secretiveusername@outlook.com>
2026-09-11 23:28:02 -0400

    I: Refactored code and added items and inventory

--- game_map.py
+++ game_map.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-from html import entities
 from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 
 import numpy as np  # type: ignore
 from tcod.console import Console
 
-from entity import Actor
+from entity import Actor, Item
 import tile_types
 
 if TYPE_CHECKING:
@@ -31,13 +30,21 @@ class GameMap:
         )  # Tiles the player has seen before
 
     @property
-    def actors(self) -> Iterator[Actor]:
+    def gamemap(self) -> GameMap:
+        return self
+
+    @property
+    def actors(self) -> Iterator[Entity | Actor]:
         """Iterate over this map's living actors."""
         yield from (
             entity
             for entity in self.entities
             if isinstance(entity, Actor) and entity.is_alive
         )
+
+    @property
+    def items(self) -> Iterator[Item]:
+        yield from (entity for entity in self.entities if isinstance(entity, Item))
 
     def get_blocking_entity_at_location(
             self, location_x: int, location_y: int,
@@ -85,5 +92,5 @@ class GameMap:
             # Only print entities that are in the FOV
             if self.visible[entity.x, entity.y]:
                 console.print(
-                    x=entity.x, y=entity.y, string=entity.char, fg=entity.color
+                    x=entity.x, y=entity.y, text=entity.char, fg=entity.color
                 )
